f4584258f524a02a4a6d1e588d53fee8787a5ca6
jtxuee@gmail.com <jtxuee@gmail.com@a9ac5a9c-960a-11de-b8e1-0bcae535652c>
2009-12-15 08:00:45 +0000

    增加内存dc

--- trunk/HDCEx.h
+++ trunk/HDCEx.h
@@ -107,4 +107,32 @@ protected:
 	HDC m_hMemDC;
 };
 
+
+class CXUEMemDC
+{
+public:
+	CXUEMemDC(HDC hDestDC, const LPRECT prcPaint);
+	virtual ~CXUEMemDC();
+	HDC	GetSafeHdc();
+	BOOL Clear(COLORREF);
+	BOOL SwapBuffer();
+	//////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////
+	void DrawDragRect(const LPRECT lpRect, SIZE size, const LPRECT lpRectLast, SIZE sizeLast, HBRUSH pBrush, HBRUSH pBrushLast);
+	void FillSolidRect(LPCRECT lpRect, COLORREF clr);
+	void FillSolidRect(int x, int y, int cx, int cy, COLORREF clr);
+	void Draw3dRect(LPCRECT lpRect,COLORREF clrTopLeft, COLORREF clrBottomRight);
+	void Draw3dRect(int x, int y, int cx, int cy, COLORREF clrTopLeft, COLORREF clrBottomRight);
+	//////////////////////////////////////////////////////////////////////////
+	HBRUSH GetHalftoneBrush();
+protected:
+	HDC			m_hDC;
+	HDC     m_hDestDC;   
+	HBITMAP m_hBitmap;    
+	RECT		m_rtPaint;      
+	HGDIOBJ m_hOldBitmap;
+};
+
+
 #endif //HWX_HDCEX_H
