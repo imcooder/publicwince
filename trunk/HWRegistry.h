55d3cf52206ef3afc378f57d6319aeca52def0ec
jtxuee@gmail.com <jtxuee@gmail.com@a9ac5a9c-960a-11de-b8e1-0bcae535652c>
2010-01-28 03:23:13 +0000

    注册表管理方面 增加删除键值

--- trunk/HWRegistry.h
+++ trunk/HWRegistry.h
@@ -21,5 +21,13 @@ DWORD		WINAPI XUE_RegQueryValueString(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszV
 BOOL		WINAPI XUE_RegSetValueBinary(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue, const BYTE* pbData, DWORD dwByte);
 DWORD		WINAPI XUE_RegQueryValueBinary(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue, const BYTE* pbDefData, DWORD dwDefSize, BYTE* pbData, DWORD dwCount);
 BOOL		WINAPI XUE_RegDeleteValue(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue);
-
+BOOL		WINAPI XUE_RegDeleteKey(HKEY hKey, LPCTSTR pszSubKey);
+BOOL		WINAPI XUE_RegDeleteAllValue(HKEY hKey, LPCTSTR pszSubKey);
+BOOL		WINAPI XUE_RegDeleteAllSubKey(HKEY hKey, LPCTSTR pszSubKey);
+BOOL		WINAPI XUE_RegIsKeyExists(HKEY hKey, LPCTSTR pszSubKey);
+BOOL		WINAPI XUE_RegIsValueExists(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue);
+BOOL		WINAPI XUE_RegIsValueDWORD(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue);
+BOOL		WINAPI XUE_RegIsValueString(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue);
+BOOL		WINAPI XUE_RegIsValueBinary(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue);
+DWORD		WINAPI XUE_RegQueryValueType(HKEY hKey, LPCTSTR pszSubKey, LPCTSTR pszValue);
 #endif//HWX_HWREGISTRY_H
