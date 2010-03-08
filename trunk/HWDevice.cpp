a2dc998d30a503954f8daf6d972a9ba026f98070
jtxuee@gmail.com <jtxuee@gmail.com@a9ac5a9c-960a-11de-b8e1-0bcae535652c>
2010-03-08 07:16:05 +0000

    获取函数ID返回错误

--- trunk/HWDevice.cpp
+++ trunk/HWDevice.cpp
@@ -2,7 +2,8 @@
 #include "stdafx.h"
 #include "HWDevice.h"
 #include "Rect.h"
-
+#include <projects.h>
+#pragma comment(lib, "note_prj.lib")
 
 #ifdef WINCE
 #include <pm.h>
@@ -141,6 +142,118 @@ LONG GetID(LPCTSTR pszDevice, LPTSTR pszID, LONG nLen)
 }
 #endif
 
+#define IOCTL_DISK_BASE           FILE_DEVICE_DISK 
+#define IOCTL_DISK_GET_STORAGEID  CTL_CODE(IOCTL_DISK_BASE, 0x709,METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+const UINT MINIMUM_CARD_SIZE = 63 * (1024 * 1024); 
+BOOL WINAPI isCardAvailable(TCHAR *szPath) 
+{ //Î´¿¼ÂÇ¶à¸ö´æ´¢¿¨
+	BOOL res = FALSE; 
+	HANDLE hFlashCard = NULL; 
+	WIN32_FIND_DATA find; 
+	BOOL loop = TRUE; 
+
+	ULARGE_INTEGER freeBytesAvailableToCaller; 
+	ULARGE_INTEGER totalNumberOfBytes; 
+	ULARGE_INTEGER totalNumberOfFreeBytes; 
+
+	memset(&find, 0, sizeof(WIN32_FIND_DATA)); 
+
+	hFlashCard = ::FindFirstFlashCard(&find); 
+
+	if(INVALID_HANDLE_VALUE != hFlashCard) 
+	{  
+		if(((find.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY) && 
+			((find.dwFileAttributes & FILE_ATTRIBUTE_TEMPORARY) == FILE_ATTRIBUTE_TEMPORARY)) 
+		{ 
+			res = ::GetDiskFreeSpaceEx(find.cFileName, &freeBytesAvailableToCaller, 
+				&totalNumberOfBytes, 
+				&totalNumberOfFreeBytes); 
+
+			// Only count the card if it is the correct size 
+			if((res == TRUE) && (totalNumberOfBytes.QuadPart >= MINIMUM_CARD_SIZE)) 
+			{ 
+				// Save the name of the flash card 
+				_tcsncpy(szPath, find.cFileName, MAX_PATH); 
+
+			} 
+		} 
+
+		::FindClose (hFlashCard); 
+	} 
+
+	return res; 
+} 
+
+
+BOOL WINAPI XUE_GetSDID2(UINT& uSDID)
+{
+	TCHAR szPath[MAX_PATH] = {0};
+
+	if (!isCardAvailable(szPath))
+	{
+		return FALSE;
+	}
+	StringCchCat(szPath, MAX_PATH, TEXT("\\Vol:"));
+	HANDLE theVolumeHandle = CreateFile(szPath, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+	BOOL success = FALSE;
+	if( theVolumeHandle != INVALID_HANDLE_VALUE )
+	{
+		// Allocate the storage info
+		DWORD theBytesReturned = 0;
+
+		int theSize = sizeof(STORAGE_IDENTIFICATION) + 33;
+		STORAGE_IDENTIFICATION *theStorageInfo = (STORAGE_IDENTIFICATION*) malloc( theSize );
+		memset( theStorageInfo, 0, theSize );
+
+		success = DeviceIoControl(
+			theVolumeHandle,
+			IOCTL_DISK_GET_STORAGEID,
+			NULL,
+			0,
+			theStorageInfo,
+			theSize - 1,
+			&theBytesReturned,
+			NULL);
+
+		if( success )
+		{
+			success = FALSE;
+
+			if( theStorageInfo->dwManufactureIDOffset > 0 )
+			{
+				// Not used.
+			}
+
+			if( theStorageInfo->dwSerialNumOffset > 0 && theStorageInfo->dwSerialNumOffset < theStorageInfo->dwSize )
+			{
+				char* theCardId = ((char*) theStorageInfo ) + theStorageInfo->dwSerialNumOffset;
+
+				// Convert string to long.
+				uSDID = strtoul( theCardId, NULL, 16 );
+				if( uSDID == 0 || uSDID == ULONG_MAX )
+				{
+					uSDID = strtoul( theCardId, NULL, 10 );
+				}
+
+				if( uSDID != 0 && uSDID != ULONG_MAX )
+				{
+					success = TRUE;
+
+				}
+			}
+		}
+
+		// Cleanup.
+
+		free( theStorageInfo );
+
+		CloseHandle( theVolumeHandle );
+	}
+	return success;
+
+}
+
 LONG WINAPI XUE_GetSDID(LPTSTR pszID, LONG nLen)
 {
 	TCHAR		szID[1024] = {0};
@@ -209,3 +322,52 @@ LONG WINAPI XUE_GetSDID(LPTSTR pszID, LONG nLen)
 	return 0;
 }
 
+BOOL WINAPI XUE_GetSDID3(UINT& uSDID)
+{
+	BOOL blRet = FALSE;
+	uSDID = 0;
+	HANDLE theVolumeHandle = CreateFile(TEXT("\\Storage Card\\Vol:"), GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);	
+	if( theVolumeHandle != INVALID_HANDLE_VALUE )
+	{
+		// Allocate the storage info
+		int theSize = sizeof(STORAGE_IDENTIFICATION) + 33;
+		STORAGE_IDENTIFICATION* theStorageInfo = (STORAGE_IDENTIFICATION*) malloc( theSize );
+		memset( theStorageInfo, 0, theSize );
+
+		DWORD theBytesReturned;
+		char* theCardId = NULL;
+		long unit_id;
+		BOOL success = DeviceIoControl(theVolumeHandle,	IOCTL_DISK_GET_STORAGEID,	NULL,	0,theStorageInfo,theSize - 1,	&theBytesReturned,NULL);
+		if( success )
+		{
+			success = FALSE;
+			if( theStorageInfo->dwManufactureIDOffset > 0 )
+			{
+				// Not used.
+			}
+			if( theStorageInfo->dwSerialNumOffset > 0 && theStorageInfo->dwSerialNumOffset < theStorageInfo->dwSize )
+			{
+				theCardId = ((char*) theStorageInfo ) + theStorageInfo->dwSerialNumOffset;
+				// Convert string to long.
+				unit_id = strtoul( theCardId, NULL, 16 );
+				if( unit_id == 0 || unit_id == ULONG_MAX )
+				{
+					unit_id = strtoul( theCardId, NULL, 10 );
+				}
+				if( unit_id != 0 && unit_id != ULONG_MAX )
+				{
+					success = TRUE;					
+					uSDID = unit_id;
+					blRet = TRUE;
+				}
+			}
+		}
+
+		// Cleanup.
+		free( theStorageInfo );
+		theStorageInfo = NULL;
+		CloseHandle( theVolumeHandle );
+		theVolumeHandle = NULL;
+	}
+	return blRet;
+}
